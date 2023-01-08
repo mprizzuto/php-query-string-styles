c8ca8f4979f8e0f42e7b629342870d2d15ae9c0e
Marco R <mprizzuto88@gmail.com>
2023-01-07 21:43:07 -0500

    add header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+
+<?php include "functions.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title></title>
+</head>
+<body>
+<header class="site-header">
+	<inner-column>
+		<?php include "./templates/components/site-menu.php"?>
+	</inner-column>
+</header>
+<main>
